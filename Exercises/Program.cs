683a1d679dc33eed3c3dc2ddf0f1fd2b7ff84297
BenStrawbridge <lute_midnight@yahoo.com>
2020-10-27 11:57:12 -0500

    Selection Statement Exercise

---
+++ Exercises/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine("What is your favorite subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math");
+                    break;
+                case "English":
+                    Console.WriteLine("English");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science");
+                    break;
+                case "History":
+                    Console.WriteLine("History");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+        }
+    }
+}
